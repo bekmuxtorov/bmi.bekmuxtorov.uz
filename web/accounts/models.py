44bba8380020a4d9d14d556ee915049da37ff1b9
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-15 12:47:54 +0500

    ➕ Web: Added AUTH_USER_MODEL

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(
+        verbose_name="Phone number",
+        max_length=20,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\+998\d{9}$',
+                message='Invalid phone number. Please enter in the format +998901234567'
+            )
+        ]
+    )
+    telegram_id = models.CharField(
+        verbose_name="Telegam ID",
+        max_length=21,
+        blank=True
+    )
+    full_name = models.CharField(
+        verbose_name="Full Name",
+        max_length=100,
+    )
+    confirm_code = models.IntegerField(
+        verbose_name="Confirm code",
+        blank=True,
+        null=True
+    )
+    is_staff = models.BooleanField(
+        verbose_name="is staff",
+        default=False
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Created time",
+        auto_now_add=True
+    )
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["full_name"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.full_name
