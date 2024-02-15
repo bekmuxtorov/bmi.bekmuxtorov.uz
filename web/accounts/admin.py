84193e11a443b1363d153bd0010f86fbdcd78582
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-15 12:54:08 +0500

    📝 Web: Added User model in the admin

--- web/accounts/admin.py
+++ web/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'full_name', 'telegram_id',
+                    'confirm_code', 'created_at', 'is_staff')
+    search_fields = ('phone_number', 'full_name', 'telegram_id')
+    list_per_page = 100
