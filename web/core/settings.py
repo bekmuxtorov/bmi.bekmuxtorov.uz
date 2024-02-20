fa28d9c4818b3999544a275c9d042b993bbd4962
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-21 00:28:32 +0500

    ➕ Web: Added templates dirs

--- web/core/settings.py
+++ web/core/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,7 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles"
+]
 
 STATIC_ROOT = BASE_DIR / 'static'
 
