4308196b7c4a790c9d7b1a82de46ee4fd2428f15
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-21 00:27:58 +0500

    ➕ Web: Added login page

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import User
+
+
+def login_view(request):
+    if request.method == 'POST':
+        confirm_code = request.POST["confirm_code"]
+
+        if not confirm_code.isdigit():
+            return render(request, "login.html", context={"status_code": 401})
+
+        is_exists_confirm_code = User.objects.filter(
+            confirm_code=confirm_code).exists()
+        if not is_exists_confirm_code:
+            return render(request, "login.html", context={"status_code": 401})
+
+        user_data = User.objects.filter(confirm_code=confirm_code).first()
+        if user_data:
+            login(request, user_data)
+            return HttpResponse("Hammasi cho'tki")
+        else:
+            redirect('login')
+    return render(request, 'login.html')
