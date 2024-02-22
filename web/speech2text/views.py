d1ef71923a867a0027c00a841d87b50ce98dad5a
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-22 23:09:05 +0500

    📝 Web: Define audio size

--- web/speech2text/views.py
+++ web/speech2text/views.py
@@ -13,9 +13,12 @@ def speech_to_text(request):
         form = AttemptRecordForm(request.POST, request.FILES)
         if form.is_valid():
             form.instance.user = request.user
+            audio_size = form.instance.audio.size
+            if audio_size >= 1000000:
+                return render(request, "stt.html", context={"status_code": 401, "form": AttemptRecordForm()})
+
             form.save()
-            print(form.instance.audio)
-            return render(request, 'stt.html', {'audio': form.instance.audio})
+            return render(request, 'stt.html', {'audio': form.instance.audio, "form": AttemptRecordForm()})
         else:
             print(form.errors)
     else:
