5da797c0bf9b1030b65a2334fdab9b6eeb4cb03d
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-22 23:07:49 +0500

    ➕ Web: Added audio field in forms

--- web/speech2text/forms.py
+++ web/speech2text/forms.py
@@ -6,3 +6,5 @@ class AttemptRecordForm(forms.ModelForm):
     class Meta:
         model = Attempt
         fields = ('user', 'audio')
+    audio = forms.FileField(
+        widget=forms.FileInput(attrs={'accept': "audio/*"}))
