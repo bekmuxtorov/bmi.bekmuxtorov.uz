0cbf9db60de02a42faa4ffc134ed4704304ef2cf
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-03-12 16:58:53 +0500

    ➕ WEB: Added audio_code field in admin.py

--- web/speech2text/admin.py
+++ web/speech2text/admin.py
@@ -4,7 +4,7 @@ from .models import Attempt
 
 @admin.register(Attempt)
 class AttemptModel(admin.ModelAdmin):
-    list_display = ("user", "audio", "created_at")
+    list_display = ("user", "audio", "audio_code", "created_at")
     search_fields = ("user__full_name", "user__telegram_id",
                      "user__phone_number")
     list_filter = ("user",)
